638606b99a6c985aed67d7e57bff1cfc1e3d5213
Nick Paladino <nickp@ncpholdings.org>
2018-04-13 16:07:31 -0500

    Updated Order class
    Made

--- Order.java
+++ Order.java
@@ -1,4 +1,15 @@
-public class Order
-{
+import java.util.Stack
 
+public class Order<T> extends Stack<T>
+{
+	public String toString()
+	{
+		String ret = new String();
+		for(T obj : this)
+		{
+    		//System.out.println(obj);
+			ret += obj;
+		}
+		return ret;
+	}
 }
