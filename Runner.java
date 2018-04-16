826dd0d7747de7344f2cd44177727778b610256c
Nick Paladino <nickp@ncpholdings.org>
2018-04-16 18:57:38 -0500

    Deleted unused variable

--- Runner.java
+++ Runner.java
@@ -4,11 +4,9 @@ public class Runner
 {
 	public static void main(String[] args)
 	{
-
-		float amountDue = 0f;
 		Order<Integer, Integer> nicksOrder = new Order<Integer, Integer>();
 
-		nicksOrder.put(0/*Egg Roll*/ , 5);
+		nicksOrder.put(0/*Egg Roll*/, 5);
 		System.out.println(nicksOrder);
 		
 		System.out.println(nicksOrder.calculateBill());
