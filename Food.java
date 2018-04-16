610e0a2b5bc587efe722ad4d5432cb37d77cd638
Nick Paladino <nickp@ncpholdings.org>
2018-04-16 14:30:28 -0500

    Project format. Would like to discuss

--- Food.java
+++ Food.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public abstract class Food
 {
   int id;
   String name;
   float price;
+
+  public static List<Food> menu = new ArrayList<Food>() {{
+    								add(new EggRoll());
+							  }};
+
 
   public int getID(){ return id; }
   public String getName(){ return name; }
