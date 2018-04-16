e239c7d9dc5a549f106c57c169567f8f0187a6f7
Nick Paladino <nickp@ncpholdings.org>
2018-04-16 16:23:56 -0500

    nonsense

--- Runner.java
+++ Runner.java
@@ -3,10 +3,17 @@ public class Runner
 {
 	public static void main(String[] args)
 	{
+
+		float amountDue = 0f;
 		Order<String, Integer> nicksOrder = new Order<String, Integer>();
-		System.out.println(Food.menu);
+		//System.out.println(Food.menu);
 
 		nicksOrder.put("Egg Roll", 5);
 		System.out.println(nicksOrder);
+
+		for (Entry<String, Integer> orderedFood : nicksOrder.entrySet())
+		{
+			//Food.menu.get()
+		}
 	}
 }
