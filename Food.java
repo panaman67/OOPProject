6ec0670ee1bf7d2948d7ef19412b4c4abfdd86f9
Nick Paladino <nickp@ncpholdings.org>
2018-04-16 16:08:36 -0500

    Added comments for possible abstract methods

--- Food.java
+++ Food.java
@@ -14,4 +14,12 @@ public abstract class Food
   public int getID(){ return id; }
   public String getName(){ return name; }
   public float getPrice(){ return price; }
+
+  /*  Example abstract methods to implement in our foods? 
+   *  will uncomment when we decide
+  public abstract String getDescription();
+  public abstract boolean isHealthy();
+  public abstract boolean isMainDish();
+  public abstract boolean isAppetizer();
+  */
 }
