488cbd3384d00b38b0cdda0a202706145a376ad9
Nick Paladino <nickp@ncpholdings.org>
2018-04-17 00:35:54 -0500

    Removed incorrect comment

--- Runner.java
+++ Runner.java
@@ -18,7 +18,7 @@ public class Runner
 		System.out.print("What would you like to order: ");
 		int choice = stdin.nextInt();
 
-		nicksOrder.put(Food.menu.get(choice).id/*Egg Roll*/, 5);
+		nicksOrder.put(Food.menu.get(choice).id, 5);
 
 		System.out.println(nicksOrder);
 		System.out.println("Your final bill is: " + nicksOrder.calculateBill());
