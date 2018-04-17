da06c925033fdc1f8d69d7ab8e7fea31fdba72f1
Nick Paladino <nickp@ncpholdings.org>
2018-04-17 00:04:49 -0500

    Slight fixes to runner code

--- Runner.java
+++ Runner.java
@@ -1,14 +1,30 @@
 import java.util.Map.*;
+import java.util.Scanner;
 
 public class Runner
 {
 	public static void main(String[] args)
 	{
+		Scanner stdin = new Scanner(System.in);
 		Order<Integer, Integer> nicksOrder = new Order<Integer, Integer>();
 
-		nicksOrder.put(0/*Egg Roll*/, 5);
-		System.out.println(nicksOrder);
+		//nicksOrder.put(Food.menu.get(0).id/*Egg Roll*/, 5);
+		//System.out.println(nicksOrder);
 		
-		System.out.println(nicksOrder.calculateBill());
+		//System.out.println(nicksOrder.calculateBill());
+
+		for (Food item : Food.menu)
+		{
+			System.out.println(item);
+		}
+
+		System.out.print("What would you like to order: ");
+		int choice = stdin.nextInt();
+
+		nicksOrder.put(Food.menu.get(choice).id/*Egg Roll*/, 5);
+
+		System.out.println(nicksOrder);
+		System.out.println("Your final bill is: " + nicksOrder.calculateBill());
+
 	}
 }
