88a2f1c4ef9b4bf1b0fa90cdac7a22266cdee3aa
Nick Paladino <nickp@ncpholdings.org>
2018-04-18 11:04:02 -0500

    Made the loop for ordering, added methods

--- Runner.java
+++ Runner.java
@@ -6,22 +6,35 @@ public class Runner
 	public static void main(String[] args)
 	{
 		Scanner stdin = new Scanner(System.in);
-		Order<Integer, Integer> nicksOrder = new Order<Integer, Integer>();
-		
-		System.out.println("Object Oriental Place Chinese Restaurant");
-		System.out.println("------------------Menu------------------");
-		for (Food item : Food.menu)
-		{
-			System.out.print(item);
-		}
+		Order<Integer, Integer> nicksOrder = new Order<Integer, Integer>();	
+		int choice = Integer.MAX_VALUE;
+		Food.printMenu();
 
 		System.out.print("What would you like to order: ");
-		int choice = stdin.nextInt();
-
-		nicksOrder.put(Food.menu.get(choice).id, 5);
+		String input = stdin.next();
+		while (!isInteger(input))
+		{
+			System.out.println("Please enter a valid id nerd!");
+			System.out.print("What would you like to order: ");
+			input = stdin.next();
+		}
+		choice = Integer.parseInt(input);
+		nicksOrder.put(Food.menu.get(choice).id, 10);
 
 		System.out.println(nicksOrder);
 		System.out.println("Your final bill is: " + nicksOrder.calculateBill());
 
+	}
+	public static boolean isInteger(String str)
+	{
+		try
+		{
+			int d = Integer.parseInt(str);
+		}
+		catch(NumberFormatException nfe)
+		{
+			return false;
+		}
+		return true;
 	}
 }
