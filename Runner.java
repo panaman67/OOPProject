fc85d18fbd57c8219072fb33a88b7b8639d19e5d
Nick Paladino <nickp@ncpholdings.org>
2018-04-18 12:44:07 -0500

    Fixed loop sort of, having error message dups, working on a fix

--- Runner.java
+++ Runner.java
@@ -8,23 +8,34 @@ public class Runner
 		Scanner stdin = new Scanner(System.in);
 		Order<Integer, Integer> nicksOrder = new Order<Integer, Integer>();	
 		int choice = Integer.MAX_VALUE;
-		Food.printMenu();
 
+		Food.printMenu();
 		System.out.print("What would you like to order: ");
-		String input = stdin.next();
-		while (!isInteger(input))
+		while (stdin.hasNextLine())
 		{
-			System.out.println("Please enter a valid id nerd!");
-			while (stdin.hasNext()){input = stdin.next();}
-			System.out.print("What would you like to order: ");
-			input = stdin.next();
+			if (stdin.hasNextInt())
+			{
+				choice = stdin.nextInt();
+				if (choice > 7 || choice < 0)
+				{
+					System.out.println("Please enter a valid choice (0-7)");
+					continue;
+				}
+				break;
+			}
+			else
+			{
+				System.out.println("Input required to be integer");
+				stdin.nextLine();
+			}
 		}
-		choice = Integer.parseInt(input);
 		nicksOrder.put(Food.menu.get(choice).id, 10);
 
 		System.out.println(nicksOrder);
-		System.out.println("Your final bill is: " + nicksOrder.calculateBill());
+		System.out.println(String.format("Your final bill is: %.2f", 
+					nicksOrder.calculateBill()));
 
+	
 	}
 	public static boolean isInteger(String str)
 	{
