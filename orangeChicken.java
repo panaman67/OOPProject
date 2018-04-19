33e3589a069151bb573ed4f71305d51c446cad82
Timothy Bell <timothy.bell@ttu.edu>
2018-04-19 17:54:56 -0500

    Added basic classes

---
+++ orangeChicken.java
@@ -0,0 +1,12 @@
+public class orangeChicken extends Food{
+  public orangeChicken{
+    id = 5;
+    name = "Orange Chicken";
+    price = 8.99f;
+  }
+
+  public String getDescription(){
+    return "fried chicken pieces coated in a sweet orange-flavored chili sauce";
+  }
+
+}
---
+++ sweetSourChicken.java
@@ -0,0 +1,12 @@
+public class sweetSourChicken extends food{
+
+    public sweetSourChicken(){
+      id = 4;
+      name = "Sweet and Sour Chicken";
+      price = 8.99f;
+    }
+
+    public String getDescription(){
+      return "Deep fried chicken paired with a red sweet and sour sauce";
+    }
+}
