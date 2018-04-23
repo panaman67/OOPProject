4a8bd9e43321428103a5c729f8de5990d77d3535
gavinhouse <37221120+gavinhouse@users.noreply.github.com>
2018-04-23 10:32:45 -0500

    Create Tofu.java

---
+++ Tofu.java
@@ -0,0 +1,11 @@
+public class Tofu extends Food{
+  
+  //constructor for default tofu object
+  public Tofu(){
+    id = 1;
+    name = "Homemade Tofu";
+    price = 2.00;
+  }
+  
+  public String getDescription(){return "Warm, fluffy tofu made fresh in house every day."}
+  
