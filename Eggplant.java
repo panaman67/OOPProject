1adb132fb81d1a93af9f8485d44d20ad50386241
gavinhouse <37221120+gavinhouse@users.noreply.github.com>
2018-04-23 10:35:37 -0500

    Create Eggplant.java

---
+++ Eggplant.java
@@ -0,0 +1,12 @@
+public class Eggplant extends Food{
+  
+  //constructor for default Eggplant object
+  public Eggplant(){
+    id = 3
+    name = "Eggplant"
+    price = 4.25;
+  }
+  
+  public String getDescription(){return "farm fresh eggplant pan-fried and served over a bed of rice"}
+  
+}
