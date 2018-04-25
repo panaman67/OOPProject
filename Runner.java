ff08b0d746a11375657916fb319026898beb3144
Nick Paladino <nickp@ncpholdings.org>
2018-04-25 10:48:21 -0500

    ACTUAL Main loop started and working

--- Runner.java
+++ Runner.java
@@ -9,40 +9,34 @@ public class Runner
 		String input = new String();
 		int amountOrdered = 0, selected = -1;
 		Order<Integer, Integer> nicksOrder = new Order<Integer, Integer>();
-
+		
 		do
 		{
+			printOptions();
+			selected = Helpers.extractUnsignedInt(stdin.nextLine());
 			switch (selected)
 			{
 				case -1:
 				case  0:
 					Food.printMenu();
 					break;
-				default:
+				case 1:
 					System.out.print("What to order (ID#): ");
 					selected = requestOrderID();
+					System.out.print("How many: ");
+					amountOrdered = requestAmount();
+					nicksOrder.put(selected, amountOrdered);
 					break;
+				default:
+					System.err.println(String.format("UR BAD%n"));
 			}
-			printOptions();
-
+			System.out.print("Anything else (Y/N): ");
+			input = stdin.nextLine();
 		} while (!input.toUpperCase().equals("N"));
-		/*		
-			do{
-				System.out.print("What to order (ID#): ");
-				selected = requestOrderID();
-				
-				System.out.print("How many: ");
-				amountOrdered = requestAmount();
 
-				nicksOrder.put(Food.menu.get(selected).id, amountOrdered);
-				
-				System.out.print("Anything else (Y/N): ");
-				input = stdin.nextLine();
-			} while (!input.toUpperCase().equals("N"));
-		*/
 		System.out.println(nicksOrder);
-		//System.out.println(String.format("Your final bill is: %.2f",
-					//nicksOrder.calculateBill()));
+		System.out.println(String.format("Your final bill is: %.2f",
+					nicksOrder.calculateBill()));
 	}
 
 	static int requestOrderID()
@@ -69,7 +63,8 @@ public class Runner
 
 	static void printOptions()
 	{
-		System.out.print("----------------Options-----------------");
-		System.out.print(String.format("%n 0: Print menu%n"));
+		System.out.println("------------Options-------------");
+		System.out.println(" 0: Print menu");
+		System.out.println(" 1: Order something");
 	}
 }
